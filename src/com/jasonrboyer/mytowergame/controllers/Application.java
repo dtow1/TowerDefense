adb4cfc243aa331988ca00a06101d0633a8c07e0
Jason <jboy81@gmail.com>
2016-09-15 18:39:12 -0700

    added an image to splash screen and removed an extra JFrame that didn't serve much purpose

--- src/com/jasonrboyer/mytowergame/controllers/Application.java
+++ src/com/jasonrboyer/mytowergame/controllers/Application.java
@@ -6,13 +6,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 /**
- * This is the main application window. It's purpose is to get initialize the game, display a startup image and credits. It is
- * just a place holder for now.
- *  
+ * This is the main application window. It's purpose is to get initialize the
+ * game, display a startup image and credits. It is just a place holder for now.
+ * 
  * @author Jason Boyer
  * @version 0.1
  * @date 11/14/2015
@@ -20,44 +29,57 @@ import javax.swing.JFrame;
  *
  */
 public class Application extends JFrame {
-    
-    private boolean startGame=true;
-    private boolean inMainPanel = true;
-    private boolean exit=false;
-    private MainFrame mainFrame;
+
+    private boolean   startGame       = true;
+    private boolean   inMainPanel     = true;
+    private boolean   exit            = false;
     private GameFrame newGame;
-    private int gameFrameID,mainPanelID;
-    private final int MAIN_FRAME_X = 300, MAIN_FRAME_Y=300;
-    private final int SPLASH_SCREEN_X = 400, SPLASH_SCREEN_Y=700;
+    private int       gameFrameID, mainPanelID;
+    private final int MAIN_FRAME_X    = 300, MAIN_FRAME_Y = 300;
+    private final int SPLASH_SCREEN_X = 400, SPLASH_SCREEN_Y = 700;
+    private String    path            = "images" + File.separator;
 
     public Application() {
-        
 
-        mainFrame = new MainFrame(startGameListener);
-        inMainPanel=false;
-        mainFrame.addWindowListener(listenToWindow);
+        inMainPanel = false;
+        setSize(SPLASH_SCREEN_X, SPLASH_SCREEN_Y);
 
-        mainFrame.setVisible(true);
-        mainFrame.setSize(MAIN_FRAME_X,MAIN_FRAME_Y);
-        setSize(SPLASH_SCREEN_X,SPLASH_SCREEN_Y);
-                
-        setTitle("TowerGame");
+        setTitle("Tower Defense Game");
         this.setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-   
+
+        new BorderLayout();
+        JPanel testPanel = new JPanel(new BorderLayout());
+        JLabel testLabel = new JLabel(
+                "<html><h1 style='font-weight: bolder'>Welcome to the Game</h1></html>");
+        testLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        BufferedImage image;
+        try {
+            image = ImageIO.read(new File(path + "towerimage.jpeg"));
+            JLabel towerImage = new JLabel(new ImageIcon(image));
+            testPanel.add(towerImage, BorderLayout.CENTER);
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        JButton startGame = new JButton("Start Game");
+        startGame.addActionListener(startGameListener);
+        testPanel.add(testLabel, BorderLayout.NORTH);
+        testPanel.add(startGame, BorderLayout.SOUTH);
+        add(testPanel);
+
     }
-    
+
     private void initUI() {
-        
+
         newGame = new GameFrame();
-        newGame.addWindowListener(listenToWindow); 
+        newGame.addWindowListener(listenToWindow);
     }
-    
- 
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -67,78 +89,76 @@ public class Application extends JFrame {
         });
 
     }
-    
-    WindowListener listenToWindow = new WindowListener() {
 
-        @Override
-        public void windowActivated(WindowEvent arg0)
-        {
-            // TODO Auto-generated method stub
-            
-        }
+    WindowListener listenToWindow    = new WindowListener() {
 
-        @Override
-        public void windowClosed(WindowEvent arg0)
-        {
-            // TODO Auto-generated method stub
-            if(arg0.getSource().equals(newGame)) {
-                mainFrame.setVisible(true);
-                setVisible(true);
-                newGame.dispose();
-            }  
-        }
+                                         @Override
+                                         public void windowActivated(
+                                                 WindowEvent arg0) {
+                                             // TODO Auto-generated method stub
 
-        @Override
-        public void windowClosing(WindowEvent arg0)
-        {
-            if(arg0.getSource().equals(newGame)) {
-                mainFrame.setVisible(true);
-                setVisible(true);
-                newGame.killTimers();
-                newGame.removeAll();
-                newGame.dispose();
-            }  
-        }
+                                         }
 
-        @Override
-        public void windowDeactivated(WindowEvent arg0)
-        {
-            
-        }
+                                         @Override
+                                         public void windowClosed(
+                                                 WindowEvent arg0) {
+                                             // TODO Auto-generated method stub
+                                             if (arg0.getSource()
+                                                     .equals(newGame)) {
+                                                 setVisible(true);
+                                                 newGame.dispose();
+                                             }
+                                         }
 
-        @Override
-        public void windowDeiconified(WindowEvent arg0)
-        {
-            // TODO Auto-generated method stub
-            
-        }
+                                         @Override
+                                         public void windowClosing(
+                                                 WindowEvent arg0) {
+                                             if (arg0.getSource()
+                                                     .equals(newGame)) {
+                                                 setVisible(true);
+                                                 newGame.killTimers();
+                                                 newGame.removeAll();
+                                                 newGame.dispose();
+                                             }
+                                         }
 
-        @Override
-        public void windowIconified(WindowEvent arg0)
-        {
-            // TODO Auto-generated method stub
-            
-        }
+                                         @Override
+                                         public void windowDeactivated(
+                                                 WindowEvent arg0) {
 
-        @Override
-        public void windowOpened(WindowEvent arg0)
-        {
-            // TODO Auto-generated method stub
-            
-        }
-        
-    };
+                                         }
 
-    
+                                         @Override
+                                         public void windowDeiconified(
+                                                 WindowEvent arg0) {
+                                             // TODO Auto-generated method stub
+
+                                         }
+
+                                         @Override
+                                         public void windowIconified(
+                                                 WindowEvent arg0) {
+                                             // TODO Auto-generated method stub
+
+                                         }
+
+                                         @Override
+                                         public void windowOpened(
+                                                 WindowEvent arg0) {
+                                             // TODO Auto-generated method stub
+
+                                         }
+
+                                     };
+
     ActionListener startGameListener = new ActionListener() {
 
-        @Override
-        public void actionPerformed(ActionEvent e)
-        {     
-            initUI();
-            mainFrame.setVisible(false);
-            setVisible(false);
-        }
-        
-    };
+                                         @Override
+                                         public void actionPerformed(
+                                                 ActionEvent e) {
+                                             initUI();
+                                             setVisible(false);
+                                         }
+
+                                     };
 }

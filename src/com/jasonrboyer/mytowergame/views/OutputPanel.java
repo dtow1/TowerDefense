e4a8d8e4b3e748619a79525e6006650bc7c52496
Jason <jboy81@gmail.com>
2016-09-17 13:02:17 -0700

    added comments to output panel

--- src/com/jasonrboyer/mytowergame/views/OutputPanel.java
+++ src/com/jasonrboyer/mytowergame/views/OutputPanel.java
@@ -6,14 +6,21 @@ import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class OutputPanel extends JPanel
-{
+/**
+ * This class is the button panel for output during gameplay. It was originally
+ * written in late 2015. I am commenting it in September of 2016. It is fairly
+ * straightforward since it is just creating labels and adding them to a panel.
+ * 
+ * @author jasonboyer
+ *
+ */
+public class OutputPanel extends JPanel {
     JLabel money;
     JLabel health;
     JLabel wave;
-    
-    public OutputPanel(){
-        this.setLayout(new GridLayout(3,1));
+
+    public OutputPanel() {
+        this.setLayout(new GridLayout(3, 1));
         money = new JLabel("Money: ");
         money.setBorder(BorderFactory.createLoweredBevelBorder());
         this.add(money);
@@ -24,21 +31,21 @@ public class OutputPanel extends JPanel
         wave.setBorder(BorderFactory.createLoweredBevelBorder());
         this.add(wave);
     }
-    
+
     public void updateMoney(int newMoney) {
         money.setText("Money: " + newMoney);
-        
+
     }
-    
+
     public void updateHealth(int newHealth) {
         health.setText("Health: " + newHealth);
     }
-    
+
     public void updateHealthandMoney(int newMoney, int newHealth) {
         money.setText("Money: " + newMoney);
         health.setText("Health: " + newHealth);
     }
-    
+
     public void setWave(int newWave) {
         wave.setText("Wave: " + newWave);
     }

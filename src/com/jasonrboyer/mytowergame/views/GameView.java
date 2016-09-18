c865bb3bb42044c127d1823e64e641814d903415
Jason <jboy81@gmail.com>
2016-09-18 13:42:16 -0700

    commented GameView

--- src/com/jasonrboyer/mytowergame/views/GameView.java
+++ src/com/jasonrboyer/mytowergame/views/GameView.java
@@ -3,20 +3,12 @@ package com.jasonrboyer.mytowergame.views;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.geom.Line2D;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
 import com.jasonrboyer.mytowergame.controllers.MapLevel;
 import com.jasonrboyer.mytowergame.models.Base;
@@ -24,149 +16,178 @@ import com.jasonrboyer.mytowergame.models.BlastAnimation;
 import com.jasonrboyer.mytowergame.models.Enemy;
 import com.jasonrboyer.mytowergame.models.Tower;
 
-public class GameView extends JPanel
-{
-    
-    
-    private int levelMap[][];
-    private MapLevel gameMap;
-    
-    final int MAP_WIDTH = 800;
-    final int MAP_HEIGHT = 400;
-    final int GRID_SIZE = 40;
-    
-    private ArrayList<Enemy> monsters;
-    private ArrayList<Tower> towers;
+/**
+ * Game view was initially created in late 2015, comments added 2016. This panel
+ * sets up the view for when the player is running a game. It controls the
+ * display of the map, enemies, towers, base, ButtonPanel and OutputPanel.
+ * 
+ * @author jasonboyer
+ *
+ */
+
+public class GameView extends JPanel {
+
+    private int                       levelMap[][];
+    private MapLevel                  gameMap;
+
+    final int                         MAP_WIDTH  = 800;
+    final int                         MAP_HEIGHT = 400;
+    final int                         GRID_SIZE  = 40;
+
+    private ArrayList<Enemy>          monsters;
+    private ArrayList<Tower>          towers;
     private ArrayList<BlastAnimation> shots;
-    
-    private String path= "images" + File.separator;
-    private Image grass,dirt,pave,base,mob,tower;
-    private Base myBase;
-    
-    public GameView(){
+
+    private String                    path       = "images" + File.separator;
+    private Image                     grass, dirt, pave, base, mob, tower;
+    private Base                      myBase;
+
+    public GameView() {
+
+        /*
+         * Setup the data structures to contain any monsters and towers that
+         * have been added. BlastAnimations have not yet been implemented.
+         */
 
         monsters = new ArrayList<Enemy>();
         towers = new ArrayList<Tower>();
-        shots=new ArrayList<BlastAnimation>();
-        
-        try
-        {
-            grass = ImageIO.read(new File(path+ "Grass.jpg"));
-            dirt = ImageIO.read(new File(path+ "Dirt.jpg"));
-            pave = ImageIO.read(new File(path+ "Pavement.jpg"));
-            tower = ImageIO.read(new File(path+ "BasicTowerSpriteSheet.png"));
-            base = ImageIO.read(new File(path+ "Base.png"));
-        } catch (IOException e)
-        {
-            // TODO Auto-generated catch block
+        // shots=new ArrayList<BlastAnimation>();
+
+        try {
+            /*
+             * load all of the basic images to be used for the map mobs and
+             * tower objects contain their own image.
+             * 
+             */
+
+            grass = ImageIO.read(new File(path + "Grass.jpg"));
+            dirt = ImageIO.read(new File(path + "Dirt.jpg"));
+            pave = ImageIO.read(new File(path + "Pavement.jpg"));
+            base = ImageIO.read(new File(path + "Base.png"));
+        }
+        catch (IOException e) {
             e.printStackTrace();
-        }//ImageIO.read(ResourceLoader.load(path+"Grass.jpg"));//
+        }
     }
+
+    /*
+     * The newLevelMap is a 2D array. This is used to create a grid for elements
+     * to be placed on. Each grid element indicates what type of tile to draw at
+     * that location. These elements are important as they are also used for
+     * navigation/pathfinding.
+     */
     public void setGameMap(MapLevel newMap, int[][] newLevelMap) {
-        gameMap=newMap;
-        levelMap=newLevelMap;
+        gameMap = newMap;
+        levelMap = newLevelMap;
     }
-    public void setEnemies(ArrayList<Enemy> enemies){
-        monsters=enemies;
+
+    /*
+     * Set the enemy list with the current number of enemies for this round.
+     */
+    public void setEnemies(ArrayList<Enemy> enemies) {
+        monsters = enemies;
     }
-    
+
+    /*
+     * Set the tower list with each tower that has been added to the map.
+     */
     public void setTowers(ArrayList<Tower> defenders) {
-        towers=defenders;
+        towers = defenders;
     }
-    
+
+    /*
+     * Not yet implemented
+     */
     public void setWeaponShots(ArrayList<BlastAnimation> firedShots) {
-        shots=firedShots;
+        shots = firedShots;
     }
-    
+
+    /*
+     * Set the graphic for the base. This changes as the base becomes damaged.
+     */
     public void setBase(Base newBase) {
-        myBase=newBase;
+        myBase = newBase;
     }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+
         drawMap(g);
 
     }
-    
 
     private void drawMap(Graphics g) {
 
-        
-        
-                       
-                        
-    //Draw Map
-        //Image dirt = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Dirt.jpg"));
-        //Image grass = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Grass.jpg"));
-        //Image pave = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Pavement.jpg"));
-        //Image mob = ImageIO.read(getClass().getClassLoader().getResourceAsStream("EnemeyRight.png"));
-        //Image tower = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Tower.jpg"));
-        //Image base = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Base.jpg"));
-       // Image dirt = ImageIO.read(new File(path+ "Dirt.jpg"));//ImageIO.read(ResourceLoader.load(path+"Dirt.jpg"));//ImageIO.read(getClass().getResourceAsStream(path + "Dirt.jpg"));//;
-        //Image grass = ImageIO.read(new File(path+ "Grass.jpg"));//ImageIO.read(ResourceLoader.load(path+"Grass.jpg"));//
-        //Image pave = ImageIO.read(new File(path+ "Pavement.jpg"));//ImageIO.read(ResourceLoader.load(path+"Pavement.jpg"));//
-        //Image mob = ImageIO.read(new File(path + "EnemyRight.png"));//ImageIO.read(ResourceLoader.load(path+"EnemyRight.png"));//;
-        //Image tower = ImageIO.read(new File(path+ "Tower.jpg"));//ImageIO.read(ResourceLoader.load(path+"Tower.jpg"));//
-        //Image base = ImageIO.read(new File(path+ "Base.jpg"));//ImageIO.read(ResourceLoader.load(path+"Base.jpg"));//
-        //buttons.setBounds(800,0,200,200);
-        //output.setBounds(0,400,200,150);
-        //output.updateHealthandMoney(myBase.getMoney(),myBase.getHealth());
-      //  buttons.setBounds(800,0,buttons.getWidth(),buttons.getHeight());
-        
-        
-        for(int i=0; i<MAP_WIDTH;i+=GRID_SIZE) {
-             for(int j=0;j<MAP_HEIGHT;j+=GRID_SIZE) {
-                 switch (levelMap[j/GRID_SIZE][i/GRID_SIZE]) {
-                      case 0:
-                      case 88:
-                         g.drawImage(grass, i, j, null);
-                         break;
-                     case 10:
-                     case 11:
-                     case 12:
-                     case 13:
-                     case 14:
-                     
-                         g.drawImage(dirt, i, j, null);
-                         break;
-                     case 20:
-                     case 21:
-                     case 22:
-                     case 23:
-                     case 24:
-                         g.drawImage(pave, i, j, null);
-                         break;
-                 }
+        /*
+         * This draws each of the tiles. The cases correspond to the correct
+         * tile type to display.
+         */
+        for (int i = 0; i < MAP_WIDTH; i += GRID_SIZE) {
+            for (int j = 0; j < MAP_HEIGHT; j += GRID_SIZE) {
+                switch (levelMap[j / GRID_SIZE][i / GRID_SIZE]) {
+                    case 0:
+                    case 88:
+                        g.drawImage(grass, i, j, null);
+                        break;
+                    case 10:
+                    case 11:
+                    case 12:
+                    case 13:
+                    case 14:
 
-             }
-         }
-        g.drawImage(myBase.getImage(),myBase.getYLocation(),myBase.getXLocation(),null);
+                        g.drawImage(dirt, i, j, null);
+                        break;
+                    case 20:
+                    case 21:
+                    case 22:
+                    case 23:
+                    case 24:
+                        g.drawImage(pave, i, j, null);
+                        break;
+                }
 
-        if(monsters.size()>0) {
-            for(int i=0; i<monsters.size();i++) {
-                Enemy newMonster=monsters.get(i);
+            }
+        }
+
+        g.drawImage(myBase.getImage(), myBase.getYLocation(),
+                myBase.getXLocation(), null);
+
+        /*
+         * if all monsters have not yet been destroyed, get each one and draw it
+         * on the map.
+         */
+        if (monsters.size() > 0) {
+            for (int i = 0; i < monsters.size(); i++) {
+                Enemy newMonster = monsters.get(i);
                 mob = newMonster.getImage();
-                g.drawImage(newMonster.getImage(), newMonster.getXLocation(), newMonster.getYLocation(),null);
+                g.drawImage(newMonster.getImage(), newMonster.getXLocation(),
+                        newMonster.getYLocation(), null);
             }
         }
-        if(towers.size()>0) {
-            for(int i=0;i<towers.size();i++) {
+
+        /*
+         * If any towers have been deployed, get them and draw them on the map
+         */
+        if (towers.size() > 0) {
+            for (int i = 0; i < towers.size(); i++) {
                 Tower newTower = towers.get(i);
-                tower=newTower.getImage();
-                g.drawImage(tower,newTower.getXLocation(), newTower.getYLocation(), null);
+                tower = newTower.getImage();
+                g.drawImage(tower, newTower.getXLocation(),
+                        newTower.getYLocation(), null);
             }
         }
-        if(shots.size()>0) {
-            Graphics2D g2 = (Graphics2D)g;
-            while(!shots.isEmpty()) {
+
+        /*
+         * If there are any weapons fired that have a shot animation, draw them
+         */
+        if (shots.size() > 0) {
+            Graphics2D g2 = (Graphics2D) g;
+            while (!shots.isEmpty()) {
                 BlastAnimation shotAnimation = shots.remove(0);
-                //g2.draw(new Line2D.Double(shotAnimation.getStartX(),shotAnimation.getStartY(),shotAnimation.getEndX(),shotAnimation.getEndY()));
             }
         }
-        
+
     }
-
-
 
 }
